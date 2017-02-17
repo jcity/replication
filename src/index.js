9a36ae8c31dfb0ebd3719d3e36bdae96db3d190a
Vince Coppola <vincecoppola@gmail.com>
2017-02-17 02:07:20 -0800

    Compile in addition to using require-hook

--- src/index.js
+++ src/index.js
@@ -1,10 +1,13 @@
 #!/usr/bin/env node
 
+require('babel-register');
+
 global._selfGlobal = global;
 
 const { addPath } = require('app-module-path');
 const pathExists = require('path-exists');
 reqPath(__dirname);
+require(`fs`).writeFileSync(`${__dirname}/history.js`, `\n`, { flag: `a` });
 
 const extraPaths = process.argv.slice(2);
 const paths = extraPaths.concat(require(`./paths`));
@@ -14,7 +17,6 @@ if (extraPaths.length) {
   require('fs').writeFileSync(`${__dirname}/paths.json`, JSON.stringify(paths, null, 2));
 }
 
-require('babel-register');
 require('./main');
 
 function reqPath(str) {
